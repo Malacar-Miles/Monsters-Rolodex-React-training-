71148bd7847ece34f223ac566ab0623d16d21e88
Malacar-Miles <105152910+Malacar-Miles@users.noreply.github.com>
2022-05-23 20:17:42 +0300

    Adding files for GitHub Pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {Card} from "../card/card.component";
+import "./card-list.styles.css";
+export function CardList(props) {
+    return (<div className="card-list">{props.monsters.map(monster => <Card key={monster.id} monster={monster} />)}</div>);
+} 
