8a81a666af8545f228cbe343b3484cdbc3616dac
Malacar-Miles <105152910+Malacar-Miles@users.noreply.github.com>
2022-05-31 04:40:49 +0300

    Reworked the App component to use hooks instead of the class component syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
+
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => setMonsters(users));
+  }, []);
+
+  const handleChange = (event) => setSearchField(event.target.value);
+
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+  return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" handleChange={handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+  );
+}
+
+/* Older code that uses a class component
 
 class App extends React.Component {
   constructor() {
@@ -32,5 +57,7 @@ class App extends React.Component {
     );
   }
 }
+*/
 
 export default App;
+
